96db2498f6b62bfb71c079b7d0d383867f3e850e
Robert R. Meyer <blue.dog.archolite@gmail.com>
2019-04-04 16:15:34 -0600

    Config uses env or file

--- configs/config_loader.py
+++ configs/config_loader.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import yaml
 
@@ -7,20 +8,24 @@ class ConfigLoader:
     config = {}
 
     def __init__(self, config_path=None):
-        if config_path:
+        my_file = Path(config_path)
+
+        if config_path and my_file.is_file():
             self.__load_config_file(config_path)
 
-        self.config['SLACK_CLIENT_TOKEN'] = os.getenv('SLACK_CLIENT_TOKEN',
-                                                      'ERROR')
-        self.config['SLACK_CLIENT_NAME'] = os.getenv('SLACK_CLIENT_NAME',
-                                                     'lighthouse')
+        else:
 
-        self.config['CHECK_MK_SERVER'] = os.getenv('CHECK_MK_SERVER', 'ERROR'),
-        self.config['CHECK_MK_USER'] = os.getenv('CHECK_MK_USER', 'ERROR'),
-        self.config['CHECK_MK_PASSWORD'] = os.getenv('CHECK_MK_PASSWORD',
-                                                     'ERROR')
+            self.config['SLACK_CLIENT_TOKEN'] = os.getenv(
+                'SLACK_CLIENT_TOKEN', 'ERROR')
+            self.config['SLACK_CLIENT_NAME'] = os.getenv(
+                'SLACK_CLIENT_NAME', 'lighthouse')
+
+            self.config['CHECK_MK_SERVER'] = os.getenv('CHECK_MK_SERVER',
+                                                       'ERROR')
+            self.config['CHECK_MK_USER'] = os.getenv('CHECK_MK_USER', 'ERROR')
+            self.config['CHECK_MK_PASSWORD'] = os.getenv(
+                'CHECK_MK_PASSWORD', 'ERROR')
 
     def __load_config_file(self, config_path):
-
         with open(config_path, 'r') as ymlfile:
             self.config = yaml.load(ymlfile)
