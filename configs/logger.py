15582094bb711979dedbc13b83ba843c4788fd9d
Robert R. Meyer <blue.dog.archolite@gmail.com>
2019-05-13 12:09:40 -0600

    First pass of RTM and V2

---
+++ configs/logger.py
@@ -0,0 +1,12 @@
+# Import the duallog package to set up simultaneous logging to screen and console.
+import duallog
+
+# Import the logging package to generate log messages.
+import logging
+
+# Set up dual logging and tell duallog where to store the logfiles.
+duallog.setup("logtest")
+
+
+class Logger:
+    jkkuu
